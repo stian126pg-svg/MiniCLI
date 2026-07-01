d717ba934ea8af0e95609b37b3ccec83065177ba
stian126jg-svg <stian1.26jg@gmail.com>
2026-07-01 13:09:35 +0200

    Created and established Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using MiniCLI.Core;
+
+var registry = new CommandRegistry();
+
+var (commandName, arguments) = CommandParser.Parse(args);
+
+if (string.IsNullOrWhiteSpace(commandName))
+{
+    Console.WriteLine("No command specified.");
+    return;
+}
+
+if (registry.TryGetCommand(commandName, out var command))
+{
+    command.Execute(arguments);
+}
+else
+{
+    Console.WriteLine($"Unknown command: {commandName}");
+}
